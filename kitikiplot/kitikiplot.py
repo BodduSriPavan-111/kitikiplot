b8b6767ddb4279c6062615b254dcb9582b400051
Your Name <n180606@rguktn.ac.in>
2024-10-20 23:01:06 +0530

    feat: added-customization-parameters

--- kitikiplot/kitikiplot.py
+++ kitikiplot/kitikiplot.py
@@ -10,7 +10,7 @@ class kitikiplot:
         self.rows= self.data.shape[0]
         self.cols= self.data.shape[1]
 
-    def plot( self, figsize= (25, 5) ):
+    def plot( self, figsize= (25, 5), title= "KitikiPlot: Intuitive Visualization for Sliding Window", xlabel= "Sliding Windows", ylabel= "Frames", xticks_rotation= 0, yticks_rotation= 0 ):
 
         fig, ax = plt.subplots( figsize= figsize)
 
@@ -29,6 +29,12 @@ class kitikiplot:
         for each_patch in patches:
             ax.add_patch( each_patch )
 
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+
+        plt.xticks( [i+1.25 for i in range(self.rows)], ['Window_'+str(i+1) for i in range(self.rows)], rotation= xticks_rotation)
+        plt.yticks( [i*2+1.5 for i in range(self.cols)], ["Frame_"+str(i) for i in range(self.cols)], rotation= yticks_rotation)
         # automatically scale the plot
         ax.relim()
         ax.autoscale_view() 
