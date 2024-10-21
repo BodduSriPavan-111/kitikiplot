4747040c371da792e220124e100676da1bf7b7b0
Your Name <n180606@rguktn.ac.in>
2024-10-21 10:39:18 +0530

    feat: added-automatic-xticks-cellwidth-windowgap

--- kitikiplot/kitiki_cell.py
+++ kitikiplot/kitiki_cell.py
@@ -3,23 +3,21 @@ from matplotlib.patches import Rectangle
 
 class KitikiCell:
 
-    def __init__(self, data, cell_width= 0.5, cell_height= 2, line_width= 1):
+    def __init__(self, data, line_width= 1):
 
         self.data= data
-        self.cell_width= cell_width
-        self.cell_height= cell_height
         self.line_width= line_width
         self.color_config= ColorConfig( data= self.data )
 
 
 
-    def create( self, x, y, each_sample, cmap= "rainbow", edge_color= "#000000" ):
+    def create( self, x, y, each_sample, cell_width, cell_height, cmap, edge_color, window_gap ):
 
         color_map= self.color_config.config( cmap= cmap, edge_color= edge_color )
 
-        return Rectangle((x+1, y*2+1),
-           width= self.cell_width, 
-           height= self.cell_height,
+        return Rectangle((window_gap*(x+1)+ cell_width*(x+1) , y*2+1),
+           width= cell_width, 
+           height= cell_height,
            facecolor= color_map[0][ each_sample[y] ],
            edgecolor= color_map[1],
            linewidth= self.line_width)
--- kitikiplot/kitikiplot.py
+++ kitikiplot/kitikiplot.py
@@ -10,7 +10,17 @@ class kitikiplot:
         self.rows= self.data.shape[0]
         self.cols= self.data.shape[1]
 
-    def plot( self, figsize= (25, 5), title= "KitikiPlot: Intuitive Visualization for Sliding Window", xlabel= "Sliding Windows", ylabel= "Frames", xticks_rotation= 0, yticks_rotation= 0 ):
+    def plot( self, figsize= (25, 5),
+              cell_width= 0.5,
+              cell_height= 2,
+              window_gap= 1,
+              cmap= "rainbow", 
+              edge_color= "#000000", 
+              title= "KitikiPlot: Intuitive Visualization for Sliding Window", 
+              xlabel= "Sliding Windows", 
+              ylabel= "Frames", 
+              xticks_rotation= 0, 
+              yticks_rotation= 0 ):
 
         fig, ax = plt.subplots( figsize= figsize)
 
@@ -24,7 +34,16 @@ class kitikiplot:
 
             for time_frame in range(self.cols):
 
-                patches.append(  self.cell.create(index, time_frame, each_sample))
+                cell_gen= self.cell.create(x= index,
+                                           y= time_frame,
+                                           each_sample= each_sample,
+                                           cell_width= cell_width,
+                                           cell_height= cell_height,
+                                           window_gap= window_gap,
+                                           edge_color= edge_color,
+                                           cmap= cmap
+                                           )
+                patches.append( cell_gen )
 
         for each_patch in patches:
             ax.add_patch( each_patch )
@@ -33,8 +52,11 @@ class kitikiplot:
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
 
-        plt.xticks( [i+1.25 for i in range(self.rows)], ['Window_'+str(i+1) for i in range(self.rows)], rotation= xticks_rotation)
-        plt.yticks( [i*2+1.5 for i in range(self.cols)], ["Frame_"+str(i) for i in range(self.cols)], rotation= yticks_rotation)
+        plt.xticks( [(i+1)*window_gap+(i+1)*cell_width+cell_width/2 for i in range(self.rows)],
+                    ['Window_'+str(i+1) for i in range(self.rows)], rotation= xticks_rotation)
+        
+        plt.yticks( [i*2+1.5 for i in range(self.cols)],
+                    ["Frame_"+str(i) for i in range(self.cols)], rotation= yticks_rotation)
         # automatically scale the plot
         ax.relim()
         ax.autoscale_view() 
