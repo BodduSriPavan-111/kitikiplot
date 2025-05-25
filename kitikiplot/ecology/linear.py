71e9d9c03d62027f2dc8f56870c02d19e3447aeb
BodduSriPavan-111 <boddusripavan111@gmail.com>
2025-05-25 22:38:06 +0530

    release: v0.2.5

--- kitikiplot/ecology/linear.py
+++ kitikiplot/ecology/linear.py
@@ -8,14 +8,14 @@ Last Modified: 25-05-2025
 
 from kitikiplot.core import KitikiPlot
 
-def plot( data, focus, focus_alpha, xlabel, ylabel, xticks_values, ytick_prefix ):
+def plot( data, focus, focus_alpha, xlabel, ylabel, xticks_values, ytick_prefix, cmap ):
 
     ktk= KitikiPlot( data= data, stride= 1, window_length= len(data) )
 
     ktk.plot(
             figsize= (20, 1),
             cell_width= 2,
-            cmap= {-200: "#00db3e", 1: "#00b7db", 2: "#4400d8", 3: "#ff7070", 4: "#b0009b"},
+            cmap= cmap,
             focus= focus,
             focus_alpha= focus_alpha,
             transpose= True,
